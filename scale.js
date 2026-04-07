a5a13fb4bbb4128cb434f9f2fad0ee8301eef8db
윤수진 <53762771+yoonsujinn@users.noreply.github.com>
2026-04-07 22:14:31 +0900

    scale

--- scale.js
+++ scale.js
@@ -1,11 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
   const wrap = document.querySelector(".wrap");
   const wrapParent = document.querySelector(".wrapParent");
-  let padding = parseFloat(getComputedStyle(wrapParent).paddingLeft) * 2;
+  let padding = parseFloat(getComputedStyle(wrapParent).paddingLeft);
   let winW = window.clientWidth || document.documentElement.clientWidth;
   let wrapW = wrap.clientWidth + padding;
-  // console.log(padding, "padding");
-  // console.log(wrapW, "wrapW");
 
   window.addEventListener("resize", () => {
     let winW = window.clientWidth || document.documentElement.clientWidth;
